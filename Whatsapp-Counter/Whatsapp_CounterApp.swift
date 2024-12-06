fd822e5d9d705b75cb48b60eda4ab9908cc1bda2
Sabricetin <sabriyazilim@gmail.com>
2024-12-06 10:15:53 +0300

    Initial Commit

---
+++ Whatsapp-Counter/Whatsapp_CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Whatsapp_CounterApp.swift
+//  Whatsapp-Counter
+//
+//  Created by Sabri Çetin on 6.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct Whatsapp_CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
